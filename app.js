854bef3b4d1a511219097c697292fb627f423938
samdd1 <109146974+samdd1@users.noreply.github.com>
2023-06-09 22:36:00 +0530

    Update app.js

--- app.js
+++ app.js
@@ -229,6 +229,6 @@ app.post("/failure",function(req,res){
 })
 
 
-app.listen(process.env.PORT || 3000,function(){
+app.listen(process.env.PORT || 3000 ,"0.0.0.0",function(){
     console.log("server is listening")
 })
